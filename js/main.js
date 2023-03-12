8532147cba1cd539a842b998fc1485bd517a8a30
Mohamed-Mos-aad <ma3268787@gmail.com>
2023-03-12 07:44:44 +0200

    forth commit

--- js/main.js
+++ js/main.js
@@ -18,8 +18,8 @@ adviceButton.addEventListener('click' , function()
     {
         var advices = await fetch(`https://api.adviceslip.com/advice`);
         var advice = await advices.json();
-        document.querySelector('#adviceId').innerHTML = `#${advice.slip.id}`;
-        document.querySelector('#advice').innerHTML = `${advice.slip.advice}`;
+        document.querySelector('#adviceId').innerHTML = `# ${advice.slip.id}`;
+        document.querySelector('#advice').innerHTML = `“${advice.slip.advice}”`;
     }
     startAdvice();
 })
