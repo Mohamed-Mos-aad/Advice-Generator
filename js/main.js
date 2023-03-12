a6673df0ebd9db220b39976de1f4424b83b9773f
Mohamed-Mos-aad <ma3268787@gmail.com>
2023-03-12 08:31:56 +0200

    fifthy commit

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ async function startAdvice()
     var advices = await fetch(`https://api.adviceslip.com/advice`);
     var advice = await advices.json();
     document.querySelector('#adviceId').innerHTML = `#${advice.slip.id}`;
-    document.querySelector('#advice').innerHTML = `${advice.slip.advice}`;
+    document.querySelector('#advice').innerHTML = `“${advice.slip.advice}”`;
 }
 
 startAdvice();
